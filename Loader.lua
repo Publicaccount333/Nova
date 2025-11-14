c9e8b69e7ddf26616dd4b82af593a8317fbc6162
Publicaccount333 <muscleafk@gmail.com>
2025-11-14 21:20:29 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://novahub-zd14.onrender.com/retrieve/278e6b842e5223b481c9b15da7029467'))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/6107ba06118c61aa70ac26d217cfe13d0d71fbf73f2ba4a96847dd41f13b3f82/download"))()
